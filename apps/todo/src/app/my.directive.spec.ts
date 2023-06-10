b213e59a17a3279f02b375cddeffdc7d9f31750a
Stefan Haas <stefan.haas.privat@gmail.com>
2023-06-10 21:31:31 +0200

    removed directives

--- apps/todo/src/app/my.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyDirective } from './my.directive';
-
-describe('MyDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MyDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- apps/todo/src/app/my.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: '[ngExampleMy]',
-  standalone: true,
-})
-export class MyDirective {
-  constructor() {}
-}
