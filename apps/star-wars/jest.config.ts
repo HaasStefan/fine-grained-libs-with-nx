dc8d3e5957b1881a9420408a298e86b63dfc13f2
Stefan Haas <stefan.haas.privat@gmail.com>
2023-06-10 22:41:32 +0200

    changed structure

--- apps/star-wars/jest.config.ts
+++ apps/star-wars/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: 'star-wars',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/star-wars',
+  coverageDirectory: '../../coverage/apps/star-wars',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
