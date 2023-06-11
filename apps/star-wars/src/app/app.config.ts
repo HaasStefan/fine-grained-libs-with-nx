a142b472ffc36f943da1f1e9bd54b1a2173473d6
Stefan Haas <stefan.haas.privat@gmail.com>
2023-06-11 19:26:40 +0200

    added preloading

--- apps/star-wars/src/app/app.config.ts
+++ apps/star-wars/src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import {
+  PreloadAllModules,
   provideRouter,
   withEnabledBlockingInitialNavigation,
+  withPreloading,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation(), withPreloading(PreloadAllModules)),
     provideHttpClient(),
   ],
 };
